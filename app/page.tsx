5ee41bf495e273e90f749025f3bea03678096471
Dino Curic <dino.c@me.com>
2024-11-16 11:27:24 +0100

    add reproduction code for issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import Image from "next/image";
+import img from "./aid.jpg";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <Image src={img} alt="Test" />
+    </div>
+  );
 }
